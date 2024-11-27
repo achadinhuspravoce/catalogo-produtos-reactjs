8510815fda2a35c602b16b99f948eee9fd58fec2
Alex Rondao <alexrondao@hotmail.com>
2024-11-26 23:45:24 -0300

    componente botao up

---
+++ src/Footer/BotaoUp.tsx
@@ -0,0 +1,12 @@
+import './BotaoUp.css'
+import topo from '../assets/topo.png'
+
+const BotaoUp = () => {
+  return (
+    <>
+        <a href="#inicio" id="topo"><img src={topo} alt="topo do site" /></a>
+    </>
+  )
+}
+
+export default BotaoUp
