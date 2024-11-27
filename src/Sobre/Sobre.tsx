7b5290ba4089597416ee4ca65ec49dc2675cc693
Alex Rondao <alexrondao@hotmail.com>
2024-11-26 23:44:55 -0300

    componente sobre

---
+++ src/Sobre/Sobre.tsx
@@ -0,0 +1,21 @@
+import './Sobre.css'
+
+const Sobre = () => {
+  return (
+    <>
+        <section id="sobre">
+            <div className="Container Flex2">
+                <div id="textoSobre">
+                    <h2>Entenda mais sobre o Achadinhus</h2>
+                    <p>Bem-vindo à nossa revista digital, um espaço dedicado para apresentar a você a melhor seleção de produtos cuidadosamente escolhidos. Aqui, você encontrará uma variedade de itens que atendem a diferentes necessidades e estilos de vida, desde novidades tecnológicas até artigos de decoração e moda.</p>
+                    <p className="fw-light fst-italic text-center">Nossa missão é conectar você aos produtos que tornam seu dia a dia mais prático e encantador.</p>
+                    <p>Além de serem soluções perfeitas para você, nossos produtos também são opções incríveis para presentear aqueles que você mais ama.</p>
+                    <p>Explore, descubra e deixe-se inspirar pela nossa coleção! Seu próximo item favorito pode estar a apenas uma página de distância.</p>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Sobre
